82d64b9f8a3e733d13ddea2288aeee1991380758
Roy <roymatero@gmail.com>
2021-10-13 09:40:43 +0300

    host on local ip

--- server.go
+++ server.go
@@ -9,13 +9,26 @@ import (
 	"strconv"
 )
 
-func runRPCserver(port *int) error {
+func getOutBoundIP() net.IP {
+	conn, err := net.Dial("tcp", "8.8.8.8:8")
 
+	if err != nil {
+		log.Fatal("Cannot get IP", err)
+	}
+
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.TCPAddr)
+
+	return localAddr.IP
+}
+
+func runRPCserver(port *int) error {
 	prog := new(Program)
 	rpc.Register(prog)
 	rpc.HandleHTTP()
 
-	l, e := net.Listen("tcp", ":"+strconv.Itoa(*port))
+	l, e := net.Listen("tcp", string(getOutBoundIP())+":"+strconv.Itoa(*port))
 
 	if e != nil {
 		log.Fatal("Listen error:", e)
