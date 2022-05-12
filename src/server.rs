2d40aeaae063437f24f48de5ad1835607df2c6c6
Roy Matero <roymatero@gmail.com>
2022-05-12 15:34:51 -0500

    fix write junk

--- src/server.rs
+++ src/server.rs
@@ -15,22 +15,24 @@ pub async fn server(addr: &str) -> Result<(), Box<dyn std::error::Error>> {
         println!("Connected to client: {}", socket.peer_addr().unwrap());
         tokio::spawn(async move {
             let mut file = file_io::create_bin_file("test_executable");
-            let mut buf = [0;1024];
+            let mut buf = [0; 1024];
 
             loop {
-                let n = match socket.read(&mut buf).await {
-                    Ok(n) if n == 0 => break,
-                    Ok(n) => n,
+                match socket.read(&mut buf).await {
+                    Ok(n) if n == 0 => {
+                        println!("EOF reached");
+                        break;
+                    }
+                    Ok(n) => {
+                        match file.write_all(&buf[0..n]) {
+                            Ok(_) => {}
+                            Err(e) => eprintln!("Problem writing to file: {}", e),
+                        };
+                    }
                     Err(e) => {
                         eprintln!("failed to read from socket; err = {:?}", e);
                         break;
                     }
-                };
-
-                println!("Read {} bytes from client", n);
-                match file.write_all(&buf) {
-                    Ok(_) => println!("File written"),
-                    Err(e) => eprintln!("Problem writing to file: {}", e),
                 };
             }
 
